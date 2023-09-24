9825011935b61a954084f51032b6ae817e89dc24
Adrian Martinez <martinaa@sunypoly.edu>
2023-09-24 18:56:19 -0400

    Testing

---
+++ src/SortingVisualizer/SortingVisualizer.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+
+class SortingVisualizer extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            // Array to be sorted
+            array: []
+        };
+    }
+
+    componentDidMount() {
+        this.resetArray();
+        
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
